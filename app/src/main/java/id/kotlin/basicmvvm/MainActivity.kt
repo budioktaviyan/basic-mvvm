b04827064d391692ebae9b8cf8105e5f895eb346
Budi Oktaviyan <budioktaviyans@gmail.com>
2019-09-30 22:54:59 +0700

    Initial basic binding action listener

--- app/src/main/java/id/kotlin/basicmvvm/MainActivity.kt
+++ app/src/main/java/id/kotlin/basicmvvm/MainActivity.kt
@@ -3,12 +3,15 @@ package id.kotlin.basicmvvm
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import com.google.android.material.snackbar.Snackbar
 import id.kotlin.basicmvvm.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+
+    val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+    binding.btnClick.setOnClickListener { Snackbar.make(binding.root, "Halo!", Snackbar.LENGTH_SHORT).show() }
   }
 }
